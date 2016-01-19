2ca1b0271051379f39c7829077c63966e788d106
marcelo.teixeira <mteixeira@rocket-internet.pt>
2016-01-19 15:23:11 +0000

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ http.createServer(function (req, res) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.end('test server\n');
 });
-console.log('Server running at http://localhost:8080/');
+console.log('Server running at http://localhost:80/');
 
 // Add headers for access client
 app.use(function (req, res, next) {
@@ -38,5 +38,5 @@ app.get('/read-json', function(req, res) {
 
     });
 });
-app.listen(8080);
-console.log('Listening on port 8080...')
+app.listen(80);
+console.log('Listening on port 80...')
